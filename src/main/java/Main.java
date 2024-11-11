cf06c159e0b99afbd1cb27da13103887f47c203e
JoshuaMcDonagh <joshuamichaelmcdonagh@gmail.com>
2024-11-11 10:48:01 +0000

    feat: implements task two

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,41 @@ import java.util.concurrent.CompletableFuture;
 
 public class Main {
     public static void main(String[] args) {
-        CompletableFuture<Void> helloWorldFuture = CompletableFuture.runAsync(() ->{
+//        Task One
+//        CompletableFuture<Void> helloWorldFuture = CompletableFuture.runAsync(() ->{
+//            try {
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            System.out.println("Hello world!");
+//        });
+//
+//        helloWorldFuture.join();
+
+        // Task Two
+        CompletableFuture<Void> helloFuture = CompletableFuture.runAsync(() -> {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+
+            System.out.println("Hello ");
+        });
+
+        CompletableFuture<Void> worldFuture = CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(5000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            System.out.println("Hello world!");
+            System.out.println("world!");
         });
 
-        helloWorldFuture.join();
+        helloFuture.join();
+        worldFuture.join();
     }
 }
