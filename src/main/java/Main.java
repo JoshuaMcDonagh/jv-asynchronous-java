61e97a7d149fcc3613115df1b62b04b2184e82fa
Sam Price <samprice@Sams-MacBook-Pro.local>
2024-11-11 12:31:09 +0000

    feat: implement task 6 (by updated task numbering system)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -70,6 +70,47 @@ public class Main {
 //        System.out.println(helloWorldCombinedFuture.join());
 
         // Task Four
+//        Random randomNumber = new Random();
+//        int threadSleepTimeHello = randomNumber.nextInt(1000, 10000);
+//        int threadSleepTimeWorld = randomNumber.nextInt(1000, 10000);
+//
+//        CompletableFuture<String> helloFuture = CompletableFuture.supplyAsync(() -> {
+//
+//            try {
+//                Thread.sleep(threadSleepTimeHello);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return "Hello ";
+//        });
+//
+//        CompletableFuture<String> worldFuture = CompletableFuture.supplyAsync(() -> {
+//            try {
+//                Thread.sleep(threadSleepTimeWorld);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//
+//            return "world!";
+//        });
+//
+//        CompletableFuture<String> helloWorldCombinedFuture = helloFuture.thenCombine(worldFuture, (f1, f2) -> f1 + f2);
+//
+//        try {
+//            System.out.println(helloWorldCombinedFuture.get(10, TimeUnit.SECONDS));
+//        } catch(InterruptedException | ExecutionException | TimeoutException e) {
+//            System.out.println(
+//                    "Threads did not return results in time.\n"
+//                    +"Hello thread took: "
+//                    +threadSleepTimeHello
+//                    +"\nWorld thread took: "
+//                    + threadSleepTimeWorld
+//            );
+//        }
+//    }
+
+        // Task Six (after the task update...)
         Random randomNumber = new Random();
         int threadSleepTimeHello = randomNumber.nextInt(1000, 10000);
         int threadSleepTimeWorld = randomNumber.nextInt(1000, 10000);
@@ -95,18 +136,10 @@ public class Main {
             return "world!";
         });
 
-        CompletableFuture<String> helloWorldCombinedFuture = helloFuture.thenCombine(worldFuture, (f1, f2) -> f1 + f2);
+        CompletableFuture<Void> helloWorldCombinedFuture = helloFuture
+                .thenCombine(worldFuture, (f1, f2) -> f1 + f2)
+                .thenAccept(System.out::println);
 
-        try {
-            System.out.println(helloWorldCombinedFuture.get(10, TimeUnit.SECONDS));
-        } catch(InterruptedException | ExecutionException | TimeoutException e) {
-            System.out.println(
-                    "Threads did not return results in time.\n"
-                    +"Hello thread took: "
-                    +threadSleepTimeHello
-                    +"\nWorld thread took: "
-                    + threadSleepTimeWorld
-            );
-        }
+        helloWorldCombinedFuture.join();
     }
 }
